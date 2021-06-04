f8620c4be7aebb636e0a41052bf329465ac7e716
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2021-06-04 18:00:16 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import theme from '../theme'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={theme.padding}>Hello world</h1>
+    </div>
+  )
+}
---
+++ src/theme.ts
@@ -0,0 +1,3 @@
+export default {
+  padding: 'p-6'
+}
